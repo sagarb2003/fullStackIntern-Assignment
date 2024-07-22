e61db146389559e178a68d5ebc0f67541349ec6f
sagarb2003 <sagarssb2003@gmail.com>
2024-07-22 15:09:08 +0530

    displaying data using useAuthSession hook

---
+++ app/api/user/route.ts
@@ -0,0 +1,64 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import jwt from 'jsonwebtoken'; 
+
+const client = new PrismaClient();
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
+
+export async function GET(req: NextRequest) {
+  const token = req.headers.get("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as { userId: number };
+    const user = await client.user.findUnique({
+      where: { id: decoded.userId },
+    });
+
+    if (user) {
+      return NextResponse.json({ user: { username: user.username } });
+    } else {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+  } catch (error) {
+    return NextResponse.json({ message: "Invalid token" }, { status: 401 });
+  }
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const { username, password } = body;
+
+  const existingUser = await client.user.findUnique({
+    where: { username },
+  });
+  let user;
+  if (existingUser) {
+    if (existingUser.password !== password) {
+      return NextResponse.json(
+        { message: "Invalid password" },
+        { status: 401 }
+      );
+    }
+    user = existingUser;
+  } else {
+    user = await client.user.create({
+      data: {
+        username,
+        password,
+      },
+    });
+  }
+  const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: "1h" });
+
+  return NextResponse.json({
+    message: "Success",
+    username: user.username,
+    token},
+    {status:200}
+  );
+}
---
+++ hooks/useAuthSession.ts
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setUser, clearAuth } from "@/redux/auth/auth.slice";
+import { RootState } from "@/redux/store";
+import axios from "axios";
+
+const useAuthSession = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state: RootState) => state.auth.user);
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        try {
+          const response = await axios.get(
+            "http://localhost:3000/api/user",
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          if (response.status === 200) {
+            dispatch(setUser(response.data.user));
+          } else {
+            dispatch(clearAuth());
+          }
+        } catch (error) {
+          dispatch(clearAuth());
+        }
+      } 
+      // else {
+      //   dispatch(clearAuth());
+      // }
+    };
+
+    checkSession();
+  }, [dispatch]);
+
+  return { user };
+};
+
+export default useAuthSession;
